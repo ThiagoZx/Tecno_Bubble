fc660b6c350459c5c5cac3d4415f44c5cac0c1d2
ThiagoZx <TheThiagoo0@gmail.com>
2015-11-19 23:47:20 -0200

    Moviment!
    Usamos o acelerômetro! Deixei comentado só para eu testar a força, já
    que estou usando física. À propósito, isso é uma coisa para ajeitar. A
    força adicionada ao corpo, massa x aceleração etc etc. Aproveita
    adiciona "Bouncy" para o player;

---
+++ Tecno_Bubble/Assets/Scripts/Player_Behaviour.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Behaviour : MonoBehaviour {
+
+	private float force;
+
+	void Update () {
+		//force = Input.acceleration.x;
+		force = Input.GetAxis ("Horizontal");
+		rigidbody2D.AddForce(new Vector2 (force * 10, 0));
+	}
+}
